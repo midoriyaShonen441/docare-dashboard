2dd88f0dbbb28d650b73f38bbc09b54354470690
“midoriyaShonen441” <khunnatta@gmail.com>
2022-09-21 16:53:19 +0700

    feat: add nginx

--- safetydashboard/01_front/settingAPI.js
+++ safetydashboard/01_front/settingAPI.js
@@ -2,7 +2,8 @@
 
 
 const httpAPI = () => {
-    const isAPI = "http://localhost:8888";
+    // const isAPI = "http://localhost:8888";
+    const isAPI = "http://backend:8888";
     return isAPI;
 }
 
