872fa159aab58e094697856bb0fff9488113cf73
pcsishun <pcsishun@gmail.com>
2022-09-29 15:23:12 +0700

    update

--- safetydashboard/01_front/settingAPI.js
+++ safetydashboard/01_front/settingAPI.js
@@ -1,9 +1,9 @@
 const httpAPI = () => {
     ///////////  For Deployment  ////////////
-    const isAPI = "http://localhost:443/backend";
+    // const isAPI = "http://localhost:443/backend";
     
     ///////////  For Local  ////////////
-    // const isAPI = "http://localhost:8888/backend";
+    const isAPI = "http://localhost:8888/backend";
     return isAPI;
 }
 
