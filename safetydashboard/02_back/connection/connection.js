88fd70949d0c7754db2c562f030503271d8a80dc
pcsishun <pcsishun@gmail.com>
2022-09-29 15:19:10 +0700

    fix: fix pull git for local

--- safetydashboard/02_back/connection/connection.js
+++ safetydashboard/02_back/connection/connection.js
@@ -4,42 +4,42 @@ require("dotenv").config();
 
 
 ///////////  For Deployment  ////////////
-const MONGO_URI = process.env.MONGO_URI;
-
-exports.connect = () => {
-    mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        auth: {
-          authSource: "admin",
-        },
-        user: "docareUser",
-        pass: "docareUser"
-    })
-    .then(() => {
-      console.log("connected to database");
-    })
-    .catch((error) => {
-      console.log("error Database connecting");
-      console.error(error);
-      process.exit(1);
-    });
-};
-
-///////////  For Local  ////////////
-// const MONGO_URI = process.env.MONGO_URI_LOCALHOST
+// const MONGO_URI = process.env.MONGO_URI;
 
 // exports.connect = () => {
-//   mongoose.connect(MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("connected to database");
-//   })
-//   .catch((error) => {
-//     console.log("error Database connecting");
-//     console.error(error);
-//     process.exit(1);
-//   });
+//     mongoose.connect(MONGO_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         auth: {
+//           authSource: "admin",
+//         },
+//         user: "docareUser",
+//         pass: "docareUser"
+//     })
+//     .then(() => {
+//       console.log("connected to database");
+//     })
+//     .catch((error) => {
+//       console.log("error Database connecting");
+//       console.error(error);
+//       process.exit(1);
+//     });
 // };
+
+///////////  For Local  ////////////
+const MONGO_URI = process.env.MONGO_URI_LOCALHOST
+
+exports.connect = () => {
+  mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((error) => {
+    console.log("error Database connecting");
+    console.error(error);
+    process.exit(1);
+  });
+};
