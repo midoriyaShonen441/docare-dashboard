9a63d5c95b16329a429dfba5f299e35599a2d4b6
“midoriyaShonen441” <khunnatta@gmail.com>
2022-10-04 10:46:54 +0700

    pull commit

--- safetydashboard/mongo_seed/import.sh
+++ safetydashboard/mongo_seed/import.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mongoimport --host db -u docareUser -p docareUser --db safetydb --collection user_infos --type json --file /mongo_seed/mock_data/user-array.json --jsonArray
-mongoimport --host db -u docareUser -p docareUser --db safetydb --collection user_infos --type json --file /mongo_seed/mock_data/staff-array.json --jsonArray
-mongoimport --host db -u docareUser -p docareUser --db safetydb --collection emergency_infos --type json --file /mongo_seed/mock_data/emergency-array.json --jsonArray
+mongoimport --host db -u docareUser -p docareUser --db safetydb --collection user_infos --type json --file /mongo_seed/mock_data/array-user.json --jsonArray
+mongoimport --host db -u docareUser -p docareUser --db safetydb --collection staff_infos --type json --file /mongo_seed/mock_data/array-staff.json --jsonArray
+mongoimport --host db -u docareUser -p docareUser --db safetydb --collection emergency_infos --type json --file /mongo_seed/mock_data/array-sos.json --jsonArray
+mongoimport --host db -u docareUser -p docareUser --db safetydb --collection device_infos --type json --file /mongo_seed/mock_data/array-device.json --jsonArray
